993afd06b02427afc35a30d1d1c33937c0189a84
KollTHOR <kollthor@users.noreply.github.com>
2026-09-09 15:13:55 +0900

    Release v1.8.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.audiostreamer"
         minSdk = 29
         targetSdk = 35
-        versionCode = 57
-        versionName = "1.8.4"
+        versionCode = 58
+        versionName = "1.8.5"
     }
 
     signingConfigs {
