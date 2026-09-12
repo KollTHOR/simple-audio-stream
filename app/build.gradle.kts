e410dcdb152d3025e569c8e9f5c3b3a35dbbc806
KollTHOR <kollthor@users.noreply.github.com>
2026-09-12 15:03:26 +0900

    Release v1.8.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.audiostreamer"
         minSdk = 29
         targetSdk = 35
-        versionCode = 58
-        versionName = "1.8.5"
+        versionCode = 59
+        versionName = "1.8.6"
     }
 
     val keystorePropsFile = rootProject.file("keystore.properties")
