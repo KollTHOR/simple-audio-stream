81f2848346a19ad85b37505504cef8e7a185cc45
KollTHOR <kollthor@users.noreply.github.com>
2026-09-15 12:53:26 +0900

    fix(build): ensure release APK is signed with fallback debug key when private keystore is not configured

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,8 @@ android {
             )
             if (hasReleaseSigning) {
                 signingConfig = signingConfigs.getByName("release")
+            } else {
+                signingConfig = signingConfigs.getByName("debug")
             }
         }
     }
